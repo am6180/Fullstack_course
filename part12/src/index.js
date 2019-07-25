25b531cd22a82dc1a24060b9546e09eccca452c2
Aditi <am6180@rit.edu>
2019-07-24 22:30:04 -0400

    Done exercises of part 1

--- part12/src/index.js
+++ part12/src/index.js
@@ -2,67 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-	const course = 'Half Stack application development'
-	
-	const parts = [
-	{
-		name: 'Fundamentals of React',
-		exercises: 10
-	},
+	const course = {
+		name: 'Half Stack application development',
+		parts: [
+		{
+			name: 'Fundamentals of react',
+			exercises: 10
+		},
+		{
+			name: 'Using props to pass data',
+			exercises: 7
+		},
+		{
+			name: 'State of a component',
+			exercises: 14
+		}
+	  ]
 
-	{
-		name: 'Using props to pass data',
-		exercises: 7
-	},
-
-	{
-		name: 'State of a component',
-		exercises: 14
-	}
-	]
-
-	const [first, second, third] = parts
-	console.log(first.name)
-	console.log(second.name)
-
-	const Header = (props) => {
-  	return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  	)
+	  
 	}
 
-	const Content = (props) => {
-  	return(
-    <div>
-    	<p>{first.name} {first.exercises}</p>
-    	<p>{second.name} {second.exercises}</p>
-    	<p>{third.name} {third.exercises}</p>
-    </div>
-  	)
-	}
+	const [first, second, third] = course.parts
 
-	const Total = (props) => {
-	return(
-	<div>
+	return (
+		<div>
+		<h1>{course.name}</h1>
+		<p>{first.name} {first.exercises}</p>
+		<p>{second.name} {second.exercises}</p>
+		<p>{third.name} {third.exercises} </p>
 		<p>Number of exercises {first.exercises + second.exercises + third.exercises}</p>
-	</div>	
-	)
-	}
+		</div>
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
-    </div>
-  )
-  
+		)
 }
-
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
